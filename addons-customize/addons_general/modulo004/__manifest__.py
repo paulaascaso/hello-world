902375d466225fcf699d0cefca146b5cca5945a5
paulaascaso <ascasopaula@gmail.com>
2021-12-15 14:03:08 +0100

    modulo004

---
+++ addons-customize/addons_general/modulo004/__manifest__.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+
+
+{
+    'name': 'Importar Contactos',
+    'version': '1.1.0',
+    'author': 'Paula Ascaso',
+    'maintainer': 'Paula Ascaso',
+    'category': 'Extra Tools',
+    'summary': 'Cargo dos contactos nuevos a partir de un fichero csv.',
+    'depends': ['base'],
+    'data': [
+        'views/view.xml',
+        'data/res.partner.csv'
+    ],
+}
