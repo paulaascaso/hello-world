c022fba7d569eddf2c22fa2c6019870337a57d00
paulaascaso <ascasopaula@gmail.com>
2021-12-17 11:52:50 +0100

    modulo006

---
+++ addons-customize/addons_general/modulo006/README.md
@@ -0,0 +1,5 @@
+# Módulo 6 - Crear reglas de registro (dominios)
+
+El Administración / Ajustes (usuario Mitchell Admin) puede Eliminar Productos.
+
+Le quito permiso para eliminar los Productos con los que se obtenga un Beneficio >= 0.
---
+++ addons-customize/addons_general/modulo006/__manifest__.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+
+
+{
+    'name': 'Reglas de Registro',
+    'version': '1.0.0',
+    'author': 'Paula Ascaso',
+    'maintainer': 'Paula Ascaso',
+    'category': 'Extra Tools',
+    'summary': 'El Administrador no puede eliminar productos que tengan un Beneficio positivo.',
+    'depends': ['base','stock'],
+    'data': [
+        'views/view.xml',
+        'security/security.xml'
+    ],
+}
